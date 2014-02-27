7ae4f2e41cc14a4569f82bd3e708947f02a2c2f1
Buck Ryan <fryan@rjmetrics.com>
2014-02-27 18:56:19 -0500

    Functional mapreduce job that gets the latest state of the world

--- src/deltalog/mr.clj
+++ src/deltalog/mr.clj
@@ -4,25 +4,39 @@
            [cascading.tap.hadoop Hfs]
            [cascading.scheme.hadoop TextDelimited TextLine]
            [cascading.tuple Fields]
-           [cascading.pipe Pipe Each]
+           [cascading.pipe Pipe Each GroupBy Every]
+           [cascading.operation.aggregator Last] ; for learning
            [cascading.flow Flow FlowDef]
            [cascading.flow.hadoop HadoopFlowConnector]
            [cascading.json.operation JSONSplitter JSONFlatten]))
 
-(defn make-fields [& fields] (Fields. (into-array fields)))
+(defn make-fields [fields] (Fields. (into-array fields)))
+
+(defn make-splitter
+  "Creates a JSON parser that will extract each field from the input
+  JSON object. The resultant fields will have the same names as they do
+  in the json object."
+  [json-paths]
+  (JSONSplitter. (make-fields json-paths) (into-array json-paths)))
 
 ; This creates a tap that will read a text file line-by-line.
 ; The resultant field will be named "line".
-(def in-tap (Hfs. (TextLine. (make-fields "line")) "example/input.json"))
-
-; This creates a JSON parser that will extract the "name" and "email"
-; from a JSON object. The resultant fields will be named "name" and "email".
-(def splitter (JSONSplitter. (make-fields "name" "email") (into-array ["name" "email"])))
+(def in-tap (Hfs. (TextLine. (make-fields ["line"])) "example/input.json"))
 
 (def out-tap (Hfs. (TextLine.) "example/output" true))
 
 (defn -main []
-  (let [parent-pipe (Pipe. "json_split")
-        splitter-pipe (Each. parent-pipe splitter)]
+  (let [json-paths ["id" "timestamp" "is-delete" "data"]
 
-    (.complete (.connect (HadoopFlowConnector.) in-tap out-tap splitter-pipe))))
+        splitter (make-splitter json-paths)
+        splitter-pipe (Each. "json_split" splitter)
+        group-pipe (GroupBy. splitter-pipe (make-fields ["id"]) (make-fields ["timestamp"]))
+        max-pipe (Every. group-pipe Fields/ALL (Last.) Fields/RESULTS)
+
+        flow-def (-> (FlowDef/flowDef)
+                     (.addSource splitter-pipe in-tap)
+                     (.addTailSink max-pipe out-tap))]
+
+    (-> (HadoopFlowConnector.)
+        (.connect flow-def)
+        (.complete))))
